9f8203644dac55a53dd975f8af82e91dfb2fd733
gautam67842 <gautammongiacrpro@gmail.com>
2026-09-12 21:01:32 +0530

    check

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,9 +2,14 @@ const { getUser } = require("../services/auth");
 
 async function restrictToLoggeinUserOnly(req, res, next) {
 
+    console.log("========== AUTH ==========");
+    console.log("Cookies:", req.cookies);
+    console.log("UID:", req.cookies?.uid);
+
     const userUid = req.cookies?.uid;
 
     if (!userUid) {
+        console.log("❌ NO COOKIE");
         return res.status(401).json({
             message: "Not logged in"
         });
@@ -12,11 +17,16 @@ async function restrictToLoggeinUserOnly(req, res, next) {
 
     const user = getUser(userUid);
 
+    console.log("JWT USER:", user);
+
     if (!user) {
+        console.log("❌ INVALID JWT");
         return res.status(401).json({
             message: "Not logged in"
         });
     }
+
+    console.log("✅ USER AUTHENTICATED");
 
     req.user = user;
 
